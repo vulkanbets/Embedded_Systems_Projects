ff329a3d0df4637f837d833d94f8f855e35af99b
Juan <jcorlandomc@yahoo.com>
2021-09-19 17:22:48 -0700

    Sep-19-2021 5:22 PM

--- baremetal_blink/Core/Src/main.c
+++ baremetal_blink/Core/Src/main.c
@@ -1,19 +1,27 @@
 #include "stm32f4xx.h"
 
-
 void delayMs(int n);
 
 int main(void)
 {
-	RCC->AHB1ENR |= 1;						// Enable GPIOA clock
+    RCC->AHB1ENR |=  1;             /* enable GPIOA clock */
 
-	GPIOA->MODER &= ~0x00000C00;			// clear pin mode
-	GPIOA->MODER |= 0x00000400;				// set pin to output mode
+    GPIOA->MODER &= ~0x00000C00;    /* clear pin mode */
+    GPIOA->MODER |=  0x00000400;    /* set pin to output mode */
 
-	while(1)
-	{
-		;
-	}
-
+    while(1)
+    {
+        GPIOA->ODR |=  0x00000020;  /* turn on LED */
+        delayMs(250);
+        GPIOA->ODR &= ~0x00000020;  /* turn off LED */
+        delayMs(250);
+    }
 }
 
+/* 16 MHz SYSCLK */
+void delayMs(int n)
+{
+    int i;
+    for (; n > 0; n--)
+        for (i = 0; i < 3195; i++);
+}
