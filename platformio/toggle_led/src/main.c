0ff16f554eb604676e1fefcc1b974d54cda30008
Juan <jcorlandomc@yahoo.com>
2021-10-23 00:42:35 -0700

    Oct-23-2021 12:42 AM

--- platformio/toggle_led/src/main.c
+++ platformio/toggle_led/src/main.c
@@ -1,23 +1,26 @@
 #include "stm32f4xx.h"
 
-int main(void)
+void mydelay(int n)
 {
-    // configure PA5 as output to drive the LED
-    RCC->AHB1ENR |=  1;             /* enable GPIOA clock */
-    GPIOA->MODER &= ~0x00000C00;    /* clear pin mode */
-    GPIOA->MODER |=  0x00000400;    /* set pin to output mode */
-    
-    // configure TIM2 to wrap around at 1 Hz
-    RCC->APB1ENR |= 1;              /* enable TIM2 clock */
-    TIM2->PSC = 1600 - 1;           /* divided by 1600 */
-    TIM2->ARR = 10000 - 1;          /* divided by 10000 */
-    TIM2->CNT = 0;                  /* clear timer counter */
-    TIM2->CR1 = 1;                  /* enable TIM2 */
-
-    while (1)
+    for (; n > 0; n--)
     {
-        while(!(TIM2->SR & 1)) {}   /* wait until UIF set */
-        TIM2->SR &= ~1;             /* clear UIF */
-        GPIOA->ODR ^= 0x00000020;   /* toggle green LED */
+        __ASM volatile("NOP");
     }
 }
+
+int main(void)
+{
+    RCC->AHB1ENR |=  1;             /* enable GPIOA clock */
+    
+    GPIOA->MODER &= ~0x00000C00;    /* clear pin mode */
+    GPIOA->MODER |=  0x00000400;    /* set pin to output mode */
+
+    while(1)
+    {
+        GPIOA->ODR |=  0x00000020;  /* turn on LED */
+        mydelay(100000);
+        GPIOA->ODR &= ~0x00000020;  /* turn off LED */
+        mydelay(100000);
+    }
+}
+
