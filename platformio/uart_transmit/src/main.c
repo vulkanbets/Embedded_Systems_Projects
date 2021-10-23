802ba40f3cdc958b2b92f4e0f8dfd05c8063ee0e
Juan <jcorlandomc@yahoo.com>
2021-10-23 16:35:58 -0700

    Oct-23-2021 4:35 PM

--- platformio/uart_transmit/src/main.c
+++ platformio/uart_transmit/src/main.c
@@ -9,9 +9,23 @@ int main(void)
     // Super loop
     while(1)
     {
-        USART2_write('Y');
+        USART2_write('H');
         USART2_write('e');
-        USART2_write('s');
+        USART2_write('l');
+        USART2_write('l');
+        USART2_write('o');
+        USART2_write(' ');
+        USART2_write('F');
+        USART2_write('r');
+        USART2_write('o');
+        USART2_write('m');
+        USART2_write(' ');
+        USART2_write('S');
+        USART2_write('T');
+        USART2_write('M');
+        USART2_write('3');
+        USART2_write('2');
+        USART2_write('!');
         USART2_write('\n');
         my_delay(700);
     }
