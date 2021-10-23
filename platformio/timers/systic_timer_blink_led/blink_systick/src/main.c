a1c26ba3d09d2880b11c72800bfda003866e8b04
Juan <jcorlandomc@yahoo.com>
2021-10-23 11:42:35 -0700

    Oct-23-2021 11:42 AM

--- platformio/timers/systic_timer_blink_led/blink_systick/src/main.c
+++ platformio/timers/systic_timer_blink_led/blink_systick/src/main.c
@@ -2,21 +2,37 @@
 
 int main(void)
 {
-    RCC->AHB1ENR |=  1;             /* enable GPIOA clock */
+    // !!!enable GPIOA clock!!!
+    // 
+    // Bit 0 GPIOAEN: IO port A clock enable
+    // This bit is set and cleared by software.
+    // 0: IO port A clock disabled
+    // 1: IO port A clock enabled
+    // 
+    RCC->AHB1ENR |=  1;
 
-    // clear pin mode
+    // !!!clear pin mode!!!
+    // 
     // Set Pin 5 MODER bits "11" and "10" to "1" and "1"
     // (Reset) but leave all other bits untouched
+    // PINA5 is the onboard Green LED
+    // on the nucleo board
+    // 
     GPIOA->MODER &= ~0x00000C00;
 
 
-    GPIOA->MODER |=  0x00000400;    /* set pin to output mode */
+    // !!!set pin to output mode!!!
+    // 
+    // 
+    // 
+    // 
+    GPIOA->MODER |=  0x00000400;    
 
 
     /* Configure SysTick */
     SysTick->LOAD = 0xFFFFFF;       /* reload with max value */
     SysTick->CTRL = 5;              /* enable it, no interrupt, use system clock */
-    
+
 
     while (1)
     {
