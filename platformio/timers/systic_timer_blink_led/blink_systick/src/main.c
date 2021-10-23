d2f471bb16ef51bff500735387e4e9593feeb437
Juan <jcorlandomc@yahoo.com>
2021-10-23 11:15:25 -0700

    Oct-23-2021 11:15 AM

--- platformio/timers/systic_timer_blink_led/blink_systick/src/main.c
+++ platformio/timers/systic_timer_blink_led/blink_systick/src/main.c
@@ -3,17 +3,24 @@
 int main(void)
 {
     RCC->AHB1ENR |=  1;             /* enable GPIOA clock */
-    GPIOA->MODER &= ~0x00000C00;    /* clear pin mode */
+
+    // clear pin mode
+    // Set Pin 5 MODER bits "11" and "10" to "1" and "1"
+    // (Reset) but leave all other bits untouched
+    GPIOA->MODER &= ~0x00000C00;
+
+
     GPIOA->MODER |=  0x00000400;    /* set pin to output mode */
+
 
     /* Configure SysTick */
     SysTick->LOAD = 0xFFFFFF;       /* reload with max value */
-    SysTick->CTRL = 5;     /* enable it, no interrupt, use system clock */
+    SysTick->CTRL = 5;              /* enable it, no interrupt, use system clock */
+    
 
     while (1)
     {
-        /* take bit 23 of SysTick current value and shift it to bit 5 
-           then write it to PortA */
+        /* take bit 23 of SysTick current value and shift it to bit 5 then write it to PortA */
         GPIOA->ODR = (SysTick->VAL >> (23 - 5)) & 0x00000020;
     }
 }
