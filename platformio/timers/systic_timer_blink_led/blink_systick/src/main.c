53f4f9947f0a325f02152875da6b12afd4702577
Juan <jcorlandomc@yahoo.com>
2021-10-23 13:44:33 -0700

    Oct-23-2021 1:44 PM

--- platformio/timers/systic_timer_blink_led/blink_systick/src/main.c
+++ platformio/timers/systic_timer_blink_led/blink_systick/src/main.c
@@ -1,7 +1,15 @@
 #include "stm32f4xx.h"
 
+// Program to toggle Green LED at
+// an interval of 1Hz. Uses SysTick
+// timer to issue interrupts and toggle
+// Internal clock runs at 16_MHz by defualt
+
 int main(void)
 {
+    // Global Disable IRQs
+    __disable_irq();
+
     // !!!enable GPIOA clock!!!
     // 
     // Bit 0 GPIOAEN: IO port A clock enable
@@ -23,20 +31,34 @@ int main(void)
 
     // !!!set pin to output mode!!!
     // 
+    // Set Pin 5 MODER bits "11" and "10" to "0" and "1"
+    // (General Purpose Output Mode) but leave all
+    // other bits untouched PIN_A5 is the onboard Green
+    // LED on the nucleo board
     // 
-    // 
-    // 
-    GPIOA->MODER |=  0x00000400;    
+    GPIOA->MODER |=  0x00000400;
 
 
-    /* Configure SysTick */
-    SysTick->LOAD = 0xFFFFFF;       /* reload with max value */
-    SysTick->CTRL = 5;              /* enable it, no interrupt, use system clock */
+    /* !!!Configure SysTick!!! */
+    // reload with number of clocks per second
+    // Set SysTick Reload Value Rgister
+    SysTick->LOAD = (16000000 - 1);
 
+    // Set initial value of the SysTick countdown register
+    SysTick->VAL = 0;
 
-    while (1)
-    {
-        /* take bit 23 of SysTick current value and shift it to bit 5 then write it to PortA */
-        GPIOA->ODR = (SysTick->VAL >> (23 - 5)) & 0x00000020;
-    }
+    // enable SysTick interrupt, use system clock
+    SysTick->CTRL = 7;
+
+    // Global Enable IRQs
+    __enable_irq();
+
+    while(1)
+        { ; }
+}
+
+void SysTick_Handler(void)
+{
+    // Toggle LED
+    GPIOA->ODR ^= 0x00000020;
 }
