2e8123cef545701638dce4cca5601341f9aab061
Juan <jcorlandomc@yahoo.com>
2021-10-24 17:54:49 -0700

    Oct-24-2021 5:54 PM

--- platformio/spi_accelerometer/src/main.c
+++ platformio/spi_accelerometer/src/main.c
@@ -1,8 +1,32 @@
 #include "stm32f4xx.h"
+#include "myuart.h"
 
 
 int main(void)
 {
-    
-    return 0;
+    USART2_init(); /* initialize USART2 */
+
+    // Super loop
+    while(1)
+    {
+        USART2_write('H');
+        USART2_write('e');
+        USART2_write('l');
+        USART2_write('l');
+        USART2_write('o');
+        USART2_write(' ');
+        USART2_write('F');
+        USART2_write('r');
+        USART2_write('o');
+        USART2_write('m');
+        USART2_write(' ');
+        USART2_write('S');
+        USART2_write('T');
+        USART2_write('M');
+        USART2_write('3');
+        USART2_write('2');
+        USART2_write('!');
+        USART2_write('\n');
+        my_delay(700);
+    }
 }
