6544cfa8d0d27b2670af6b09a6903d520f6264e2
Jabberwocky <mrfpgapro@gmail.com>
2023-04-08 18:58:16 -0700

    Testing esp32 & spi bus with UNO.

--- esp32/Blink Test/src/main.cpp
+++ esp32/Blink Test/src/main.cpp
@@ -1,25 +1,18 @@
 #include <Arduino.h>
-#include <SPI.h>
+
+// Set LED_BUILTIN if undefined or not pin 13
+#define LED_BUILTIN 2
 
 void setup()
 {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-
-  // initialize the SPI library
-  SPI.begin();
-
-  // Initialize Serial communication at 9600 BAUD
-  Serial.begin(9600);
 }
 
 void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
   delay(500); // wait for a half-second
-
-  // Send UART message
-  Serial.print("UART Message Sent!\n");
 
   digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
   delay(500); // wait for a half-second
