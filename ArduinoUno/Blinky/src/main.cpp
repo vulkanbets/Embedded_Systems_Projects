4bf8f84984036c2e6fbc5e2856a1dc62bd363cc6
Jabberwocky <mrfpgapro@gmail.com>
2023-04-08 17:54:20 -0700

    Message sent through UART.

--- ArduinoUno/Blinky/src/main.cpp
+++ ArduinoUno/Blinky/src/main.cpp
@@ -4,13 +4,19 @@ void setup()
 {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // Initialize Serial communication at 9600 BAUD
+  Serial.begin(9600);
 }
 
 void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(2000); // wait for a second
+  delay(500); // wait for a half-second
+
+  // Send UART message
+  Serial.print("UART Message Sent!\n");
 
   digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
-  delay(2000); // wait for a second
+  delay(500); // wait for a half-second
 }
