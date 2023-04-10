eb5a2d987f4919595c3e5eb458d45c1af4547020
Jabberwocky <mrfpgapro@gmail.com>
2023-04-09 18:29:55 -0700

    Start to incorporate "Chip Select" pin.

--- pico/pioExample/src/main.cpp
+++ pico/pioExample/src/main.cpp
@@ -1,23 +1,24 @@
 #include <Arduino.h>
-#include <stdio.h>
 #include "hardware/pwm.h"
 #include "hardware/clocks.h"
 
 // Define the GPIO pin number for the PWM signal
-const uint pwm_pin = 15;
+const uint pwm_pin = 14;
 
 // Define the PWM signal frequency and duty cycle
-const uint32_t pwm_frequency = 175000; // 175 kHz
+const uint32_t pwm_frequency = 150000; // 150 kHz
 const float duty_cycle = 50.0; // 50%
 
 // Defines the top value for the PWM counter
 const uint16_t PWM_WRAP = 8;
 
+// Get the slice number of the pwm pin
+uint slice_num = pwm_gpio_to_slice_num(pwm_pin);
+
 void setup()
 {
   // Configure the PWM signal
   gpio_set_function(pwm_pin, GPIO_FUNC_PWM);
-  uint slice_num = pwm_gpio_to_slice_num(pwm_pin);
 
   // Set the PWM frequency
   pwm_set_clkdiv(slice_num, (float)clock_get_hz(clk_sys) / (pwm_frequency * (1 << PWM_WRAP)));
@@ -27,10 +28,12 @@ void setup()
   uint16_t level = (uint16_t)(wrap_value * (duty_cycle / 100.0));
   pwm_set_wrap(slice_num, wrap_value);
   pwm_set_chan_level(slice_num, pwm_gpio_to_channel(pwm_pin), level);
-  pwm_set_enabled(slice_num, true);
 }
 
 void loop()
 {
-  tight_loop_contents();
+  pwm_set_enabled(slice_num, true);
+  delay(1000);
+  pwm_set_enabled(slice_num, false);
+  delay(1000);
 }
